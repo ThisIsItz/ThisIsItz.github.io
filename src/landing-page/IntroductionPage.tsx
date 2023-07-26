15d29f1a9d7322def0e98d3304363901d047c316
Itziar Urbieta Leon <itziarurbieta94@gmail.com>
2023-07-26 19:54:18 +0900

    feat(main): add platforms to Introductionpage

--- src/landing-page/IntroductionPage.tsx
+++ src/landing-page/IntroductionPage.tsx
@@ -1,5 +1,22 @@
-import { Button, Flex, Text } from '@chakra-ui/react'
+import { Box, Button, Flex, Link, Text } from '@chakra-ui/react'
 import React from 'react'
+import LinkedinIcon from '../icons/LinkedinIcon.tsx'
+import GithubIcon from '../icons/GithubIcon.tsx'
+import TwitterIcon from '../icons/TwiterIcon.tsx'
+
+const platforms = [
+  {
+    icon: <LinkedinIcon />,
+    href: 'https://www.linkedin.com/in/itziarurbieta/'
+  },
+  {
+    icon: <GithubIcon />,
+    href: 'https://github.com/ThisIsItz'
+  },
+  { icon: <TwitterIcon />, href: 'https://twitter.com/ItUnderTheSea' }
+]
+
+const defaultColor = '#f2bb16'
 
 export default function IntroductionPage() {
   return (
@@ -10,14 +27,46 @@ export default function IntroductionPage() {
       color="white"
       zIndex="10"
       fontWeight="600"
+      mt="-70px"
     >
       <Flex>
         Hello, I'm
-        <Text color="#f2bb16" ml="10px">
+        <Text color={defaultColor} ml="10px">
           Itziar Urbieta.
         </Text>
       </Flex>
       I'm a remote frontend developer.
+      <Flex
+        gap="20px"
+        justifyContent="center"
+        position="absolute"
+        bottom="100px"
+        ml="auto"
+        mr="auto"
+        left="0"
+        right="0"
+      >
+        {platforms.map((platform) => {
+          return (
+            <Flex
+              borderRadius="100px"
+              border={`2px solid ${defaultColor}`}
+              w="50px"
+              h="50px"
+              justifyContent="center"
+              alignItems="center"
+              _hover={{
+                bg: 'white',
+                svg: { fill: defaultColor, stroke: defaultColor }
+              }}
+            >
+              <Link href={platform.href} isExternal>
+                {platform.icon}
+              </Link>
+            </Flex>
+          )
+        })}
+      </Flex>
     </Flex>
   )
 }

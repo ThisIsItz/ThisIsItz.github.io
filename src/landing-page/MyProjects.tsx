d07191d94c23ef084bb05fb826865c3c4c9b8256
Itziar Urbieta Leon <itziarurbieta94@gmail.com>
2023-08-25 00:22:07 +0900

    create basic section for my projects

--- src/landing-page/MyProjects.tsx
+++ src/landing-page/MyProjects.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
-import { Box } from '@chakra-ui/react'
+import { Box, Flex, Text } from '@chakra-ui/react'
 
 export default function MyProjects() {
   return (
-    <Box id="my-projects" h="100vh">
-      My projects
-    </Box>
+    <Flex id="my-projects" h="100vh" m="auto" flexDir="column">
+      <Text pt="40px" justifyContent="center" marginX="auto">
+        My projects
+      </Text>
+      <Flex flexDir="column" gap="20px" mt="30px">
+        <Flex gap="30px" justifyContent="center">
+          <Box border="1px solid red">Project 1</Box>
+          <Box border="1px solid green">Project 2</Box>
+        </Flex>
+        <Flex gap="30px" justifyContent="center">
+          <Box border="1px solid red">Project 1</Box>
+          <Box border="1px solid green">Project 2</Box>
+        </Flex>
+        <Flex gap="30px" justifyContent="center">
+          <Box border="1px solid red">Project 1</Box>
+          <Box border="1px solid green">Project 2</Box>
+        </Flex>
+      </Flex>
+    </Flex>
   )
 }
